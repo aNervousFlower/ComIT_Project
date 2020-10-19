ef0d684c17c3f8d536a74ecfe36a7c770edd94c7
aNervousFlower <anervousflower@gmail.com>
2020-10-19 17:11:28 -0600

    Establish the structure for the player's turn

--- Ten Penny Game/Assets/Scripts/PlayerState.cs
+++ Ten Penny Game/Assets/Scripts/PlayerState.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class PlayerState
 {
     public Text pennyCountText;
+    public Text playerActionsInfoText;
     public int pennyCount = 10;
+    public List<string> playerActions {get;}
     private PlayerHand playerHand;
     private PlayerTable playerTable;
 
@@ -16,6 +18,21 @@ public class PlayerState
         this.pennyCountText.text = this.pennyCount.ToString();
         this.playerHand = playerHand;
         this.playerTable = playerHand.playerTable;
+        this.playerActionsInfoText = GameObject.Find("PlayerActionsInfo").GetComponent<Text>();
+        this.playerActions = new List<string>();
+        this.playerActions.AddRange(new List<string> {"Buy", "Draw"});
+        SetActionText();
+    }
+
+    private void SetActionText()
+    {
+        List<string> split = new List<string>() {"", " or ", ", "};
+        string text = "";
+        for (int index = 0; index < this.playerActions.Count; index++)
+        {
+            text = this.playerActions[index] + split[index] + text;
+        }
+        this.playerActionsInfoText.text = text;
     }
 
     public bool SpendPenny()
@@ -49,6 +66,89 @@ public class PlayerState
 
     public void NewRound()
     {
+        this.playerActions.Clear();
+        if (this.pennyCount > 0)
+        {
+            this.playerActions.Add("Buy");
+        }
+        this.playerActions.Add("Draw");
         this.playerTable.NewRound();
+        SetActionText();
+    }
+
+    public bool Draw()
+    {
+        if (!this.playerActions.Contains("Draw"))
+        {
+            return false;
+        }
+        else
+        {
+            this.playerActions.Clear();
+            this.playerActions.Add("Discard");
+            this.playerActions.Add("Play");
+            SetActionText();
+            return true;
+        }
+    }
+
+    public bool Buy()
+    {
+        if (!this.playerActions.Contains("Buy"))
+        {
+            return false;
+        }
+        else
+        {
+            this.playerActions.Clear();
+            this.playerActions.Add("Discard");
+            this.playerActions.Add("Play");
+            if (this.pennyCount > 1)
+            {
+                this.playerActions.Add("Buy");
+            }
+            SetActionText();
+            return true;
+        }
+    }
+
+    public bool CanPlay()
+    {
+        return this.playerActions.Contains("Play");
+    }
+
+    public bool Play()
+    {
+        if (!CanPlay())
+        {
+            return false;
+        }
+        else
+        {
+            this.playerActions.Clear();
+            this.playerActions.Add("Discard");
+            this.playerActions.Add("Play");
+            SetActionText();
+            return true;
+        }
+    }
+
+    public bool Discard()
+    {
+        if (!this.playerActions.Contains("Discard"))
+        {
+            return false;
+        }
+        else
+        {
+            this.playerActions.Clear();
+            if (this.pennyCount > 0)
+            {
+                this.playerActions.Add("Buy");
+            }
+            this.playerActions.Add("Draw");
+            SetActionText();
+            return true;
+        }
     }
 }
--- Ten Penny Game/Assets/Tests/EditMode_Tests/PlayerState_Tests.cs
+++ Ten Penny Game/Assets/Tests/EditMode_Tests/PlayerState_Tests.cs
@@ -15,26 +15,122 @@ namespace Tests
             GameObject gameOb = new GameObject();
             gameOb.name = "PlayerPenniesCount";
             gameOb.AddComponent<Text>();
+            GameObject gameOb2 = new GameObject();
+            gameOb2.name = "playerActionsInfoText";
+            gameOb2.AddComponent<Text>();
             PlayerHand hand = new GameObject().AddComponent<PlayerHand>();
 
             PlayerState state = new PlayerState(hand);
-            Assert.AreEqual(state.pennyCount, 10);
-            Assert.AreEqual(state.pennyCountText.text, "10");
+            Assert.AreEqual(10, state.pennyCount);
+            Assert.AreEqual("10", state.pennyCountText.text);
 
             Assert.IsTrue(state.SpendPenny());
-            Assert.AreEqual(state.pennyCount, 9);
-            Assert.AreEqual(state.pennyCountText.text, "9");
+            Assert.AreEqual(9, state.pennyCount);
+            Assert.AreEqual("9", state.pennyCountText.text);
             
             state.pennyCount = 1;
             state.pennyCountText.text = "1";
 
             Assert.IsTrue(state.SpendPenny());
-            Assert.AreEqual(state.pennyCount, 0);
-            Assert.AreEqual(state.pennyCountText.text, "0");
+            Assert.AreEqual(0, state.pennyCount);
+            Assert.AreEqual("0", state.pennyCountText.text);
 
             Assert.IsFalse(state.SpendPenny());
-            Assert.AreEqual(state.pennyCount, 0);
-            Assert.AreEqual(state.pennyCountText.text, "0");
+            Assert.AreEqual(0, state.pennyCount);
+            Assert.AreEqual("0", state.pennyCountText.text);
+        }
+
+        [Test]
+        public void TurnStructure_Test()
+        {
+            GameObject gameOb = new GameObject();
+            gameOb.name = "PlayerPenniesCount";
+            gameOb.AddComponent<Text>();
+            GameObject gameOb2 = new GameObject();
+            gameOb2.name = "playerActionsInfoText";
+            gameOb2.AddComponent<Text>();
+            PlayerHand hand = new GameObject().AddComponent<PlayerHand>();
+
+            PlayerState state = new PlayerState(hand);
+            Assert.AreEqual(2, state.playerActions.Count);
+            Assert.Contains("Draw", state.playerActions);
+            Assert.Contains("Buy", state.playerActions);
+            Assert.AreEqual("Draw or Buy", state.playerActionsInfoText.text);
+            Assert.AreEqual(10, state.pennyCount);
+
+            // tests result of Play returning false
+            Assert.IsFalse(state.Play());
+            Assert.AreEqual(2, state.playerActions.Count);
+            Assert.Contains("Draw", state.playerActions);
+            Assert.Contains("Buy", state.playerActions);
+            Assert.AreEqual("Draw or Buy", state.playerActionsInfoText.text);
+
+            // tests result of Discard returning false
+            Assert.IsFalse(state.Discard());
+            Assert.AreEqual(2, state.playerActions.Count);
+            Assert.Contains("Draw", state.playerActions);
+            Assert.Contains("Buy", state.playerActions);
+            Assert.AreEqual("Draw or Buy", state.playerActionsInfoText.text);
+
+            // tests result of Draw returning true
+            Assert.IsTrue(state.Draw());
+            Assert.AreEqual(2, state.playerActions.Count);
+            Assert.Contains("Play", state.playerActions);
+            Assert.Contains("Discard", state.playerActions);
+            Assert.AreEqual("Play or Discard", state.playerActionsInfoText.text);
+
+            // tests result of Draw returning false
+            Assert.IsFalse(state.Draw());
+            Assert.AreEqual(2, state.playerActions.Count);
+            Assert.Contains("Play", state.playerActions);
+            Assert.Contains("Discard", state.playerActions);
+            Assert.AreEqual("Play or Discard", state.playerActionsInfoText.text);
+
+            // tests result of Buy returning false
+            Assert.IsFalse(state.Buy());
+            Assert.AreEqual(2, state.playerActions.Count);
+            Assert.Contains("Play", state.playerActions);
+            Assert.Contains("Discard", state.playerActions);
+            Assert.AreEqual("Play or Discard", state.playerActionsInfoText.text);
+
+            // tests result of Discard returning true (with pennies > 0)
+            Assert.IsTrue(state.Discard());
+            Assert.AreEqual(2, state.playerActions.Count);
+            Assert.Contains("Buy", state.playerActions);
+            Assert.Contains("Draw", state.playerActions);
+            Assert.AreEqual("Draw or Buy", state.playerActionsInfoText.text);
+
+            // tests result of Buy returning true (with pennies > 0)
+            Assert.IsTrue(state.Buy());
+            Assert.AreEqual(3, state.playerActions.Count);
+            Assert.Contains("Buy", state.playerActions);
+            Assert.Contains("Play", state.playerActions);
+            Assert.Contains("Discard", state.playerActions);
+            Assert.AreEqual("Buy, Play or Discard", state.playerActionsInfoText.text);
+
+            // tests result of Play returning true
+            Assert.IsTrue(state.Play());
+            Assert.AreEqual(2, state.playerActions.Count);
+            Assert.Contains("Play", state.playerActions);
+            Assert.Contains("Discard", state.playerActions);
+            Assert.AreEqual("Play or Discard", state.playerActionsInfoText.text);
+
+            state.Discard();
+
+            state.pennyCount = 1;
+            // tests result of Buy returning true (with pennies == 1)
+            Assert.IsTrue(state.Buy());
+            Assert.AreEqual(2, state.playerActions.Count);
+            Assert.Contains("Play", state.playerActions);
+            Assert.Contains("Discard", state.playerActions);
+            Assert.AreEqual("Play or Discard", state.playerActionsInfoText.text);
+
+            state.pennyCount = 0;
+            // tests result of Discard returning true (with pennies == 0)
+            Assert.IsTrue(state.Discard());
+            Assert.AreEqual(1, state.playerActions.Count);
+            Assert.Contains("Draw", state.playerActions);
+            Assert.AreEqual("Draw", state.playerActionsInfoText.text);
         }
     }
 }
