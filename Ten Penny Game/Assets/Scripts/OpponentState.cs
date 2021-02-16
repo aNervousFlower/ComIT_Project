4f3815ca1860c9a27e4c8e9c125bcc2a528354e6
aNervousFlower <anervousflower@gmail.com>
2021-02-16 00:09:36 -0600

    implement score tracking for opponent

--- Ten Penny Game/Assets/Scripts/OpponentState.cs
+++ Ten Penny Game/Assets/Scripts/OpponentState.cs
@@ -6,18 +6,40 @@ using UnityEngine.UI;
 
 public class OpponentState
 {
+    public Text opponentScoreText;
+    public int opponentScore = 0;
     public OpponentHand opponentHand {get;}
     public OpponentTable opponentTable {get;}
     
     public OpponentState(OpponentHand opponentHand)
     {
+        this.opponentScoreText = GameObject.Find("OpponentScoreText").GetComponent<Text>();
         this.opponentHand = opponentHand;
         this.opponentTable = opponentHand.opponentTable;
     }
 
     public void NewRound()
     {
+        TallyPlayerScore();
         this.opponentHand.NewRound();
         this.opponentTable.NewRound();
+    }
+
+    public void TallyPlayerScore()
+    {
+        int positivePoints = 0;
+        int negativePoints = 0;
+        foreach(string card in this.opponentTable.cardList)
+        {
+            positivePoints += PlayerState.GetCardPoints(card);
+        }
+
+        foreach(string card in this.opponentHand.cardList)
+        {
+            negativePoints += PlayerState.GetCardPoints(card);
+        }
+        
+        this.opponentScore += positivePoints - negativePoints;
+        this.opponentScoreText.text = "Score\n" + this.opponentScore;
     }
 }
